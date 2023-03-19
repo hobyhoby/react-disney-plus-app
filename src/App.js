4c2f2f7cbac38993aa05227f7020ef050081c5c4
hobyhoby <hoby@nate.com>
2023-03-19 19:13:50 +0900

    모달 및 이미지 및 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
+import DetailPage from "./pages/DetailPage";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
 
-function App() {
+const Layout = () => {
   return (
     <div>
       <Nav />
+
+      <Outlet />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
